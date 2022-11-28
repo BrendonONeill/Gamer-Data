9faf16f4b7cd5359db93e1343c9e762a10291719
Brendon O'Neill <35762480+BrendonONeill@users.noreply.github.com>
2022-11-28 18:50:51 +0000

    Update Pages.js

--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -11,10 +11,10 @@ function Pages() {
   return (
     <>
       <Routes>
-        <Route path="games/" element={<Main />} />
-        <Route path="games/Login" element={<Login />} />
+        <Route path="/games/" element={<Main />} />
+        <Route path="/games/Login" element={<Login />} />
         <Route path="/games-id/:word" element={<Search />} />
-        <Route path="games-user/User-Games" element={<Usergames />} />
+        <Route path="/games-user/User-Games" element={<Usergames />} />
         <Route path="/games-genre/:genre" element={<Genre />} />
         <Route path="/games-info/:id" element={<GamesInfo />} />
         <Route path="/games-userp/profile" element={<User />} />
